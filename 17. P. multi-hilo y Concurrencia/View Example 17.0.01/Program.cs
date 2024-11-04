60174146009d401e55836635dff0b794d4d8ec92
Juan Beltrame <juan_beltrame@hotmail.com>
2024-11-04 11:45:19 -0300

    Updates

--- 17. P. multi-hilo y Concurrencia/View Example 17.0.01/Program.cs
+++ 17. P. multi-hilo y Concurrencia/View Example 17.0.01/Program.cs
@@ -22,23 +22,52 @@ internal class Program
         tareas.Enqueue(() => EjecutarTarea("SINC-N"));
 
         foreach (Action tarea in tareas)
-            tarea();
+            tarea!.Invoke();
+    }
+
+    public static void EjecutarEnParalelo()
+    {
+        Queue<Action> tareas = new();
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-A"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-B"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-C"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-D"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-E"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-F"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-G"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-H"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-I"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-J"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-K"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-L"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-M"));
+        tareas.Enqueue(() => EjecutarTarea("PARALLEL-N"));
+
+        foreach (Action tarea in tareas)
+            Task.Run(tarea);
     }
 
     public static void EjecutarTarea(string codigo)
     {
         int tiempo = random.Next(1000, 5000);
         Thread.Sleep(tiempo);
-        Console.WriteLine($"{DateTime.Now:T} - Tarea {codigo} completada en {tiempo} ms.");
+        Console.WriteLine($"{DateTime.Now:T} - Hilo N: {Task.CurrentId}- Tarea {codigo} completada en {tiempo} ms.");
     }
 
     private static void Main(string[] args)
     {
         random = new Random();
 
-        EjecutarSincronico();
+        //Task hilo = new(EjecutarSincronico);
+        //hilo.Start();
+
+        // Instanciar e iniciar un hilo al mismo tiempo // Me ahorra el new y el start()
+        Task hilo = Task.Run(EjecutarSincronico);
+
+        EjecutarEnParalelo();
+
+        hilo.Wait();
 
         Console.WriteLine("Finalizaron todas las tareas!");
-
     }
 }
