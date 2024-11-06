da567772c70cd7bdfb10092fdfae48e2ff326dd7
Juan Beltrame <juan_beltrame@hotmail.com>
2024-11-06 16:35:22 -0300

    Uopdate

---
+++ 17. P. multi-hilo y Concurrencia/17.1.02 Library/Caja.cs
@@ -0,0 +1,22 @@
+﻿namespace _17._1._02_Library
+{
+    public class Caja
+    {
+        public delegate void DelegadoClienteAtendido(Caja caja, string cliente);
+        private static Random random;
+        private Queue<string>? clientesALaEspera;
+        private string? nombreCaja;
+
+        static Caja() => random = new();
+
+        public Caja(string? nombreCaja, DelegadoClienteAtendido delegadoClienteAtendido)
+        {
+            clientesALaEspera = new();
+            this.nombreCaja = nombreCaja;
+        }
+
+        public string NombreCaja => nombreCaja!;
+
+        public int CantidadDeClientesALaEspera => clientesALaEspera!.Count;
+    }
+}
--- 17. P. multi-hilo y Concurrencia/17.1.02 Library/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace _17._1._02_Library
-{
-    public class Class1
-    {
-
-    }
-}
