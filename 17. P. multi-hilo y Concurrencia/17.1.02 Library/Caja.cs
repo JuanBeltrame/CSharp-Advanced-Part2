97771b3f3ce4f230d6ad7a6ec85a8a0829b626a7
Juan Beltrame <juan_beltrame@hotmail.com>
2024-11-07 16:55:08 -0300

    Update

--- 17. P. multi-hilo y Concurrencia/17.1.02 Library/Caja.cs
+++ 17. P. multi-hilo y Concurrencia/17.1.02 Library/Caja.cs
@@ -3,6 +3,7 @@
     public class Caja
     {
         public delegate void DelegadoClienteAtendido(Caja caja, string cliente);
+        private DelegadoClienteAtendido delegadoClienteAtendido;
         private static Random random;
         private Queue<string>? clientesALaEspera;
         private string? nombreCaja;
@@ -13,10 +14,28 @@
         {
             clientesALaEspera = new();
             this.nombreCaja = nombreCaja;
+            this.delegadoClienteAtendido = delegadoClienteAtendido;
         }
 
         public string NombreCaja => nombreCaja!;
 
         public int CantidadDeClientesALaEspera => clientesALaEspera!.Count;
+
+        internal void AgregarCliente(string cliente) => clientesALaEspera!.Enqueue(cliente);
+        
+        internal Task IniciarAtencion() => Task.Run(AtenderClientes);
+       
+        private void AtenderClientes()
+        {
+            do
+            {
+                if (clientesALaEspera!.Any())
+                {
+                    string cliente = clientesALaEspera!.Dequeue();
+                    delegadoClienteAtendido?.Invoke(this, cliente);
+                    Thread.Sleep(random.Next(1000, 5000));
+                }
+            } while (true);
+        }
     }
 }
---
+++ 17. P. multi-hilo y Concurrencia/17.1.02 Library/Negocio.cs
@@ -0,0 +1,50 @@
+﻿using NameGenerator.Generators;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17._1._02_Library
+{
+    public class Negocio
+    {
+        private static RealNameGenerator? realNameGenerator;
+        private ConcurrentQueue<string> clientes;
+        private List<Caja> cajas;
+
+        static Negocio() => realNameGenerator = new();
+
+        public Negocio (List<Caja> cajas)
+        {
+            this.cajas = cajas;
+            clientes = new();
+        }
+
+        public void ComenzarAtencion()
+        {
+            foreach(Caja caja in cajas)
+                caja.IniciarAtencion();
+
+            Task taskSimulacionClientes = Task.Run(GenerarClientes);
+            Task taskAsignacionCajas = Task.Run(AsignarCajas);
+        }
+
+        private void GenerarClientes()
+        {
+            do
+            {
+                string cliente = realNameGenerator!.Generate();
+                clientes.Enqueue(cliente);
+            }
+            while (true);
+        }
+
+        private void AsignarCajas()
+        {
+            cajas.OrderBy(c => c.CantidadDeClientesALaEspera);
+            Caja caja = cajas.FirstOrDefault();
+        }
+    }
+}
