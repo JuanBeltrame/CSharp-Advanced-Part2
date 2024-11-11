203904e96ab61bdb1a5cc1622cc34a28dbf33899
Juan Beltrame <juan_beltrame@hotmail.com>
2024-11-11 16:43:34 -0300

    u

--- 17. P. multi-hilo y Concurrencia/View Example 17.0.03/Program.cs
+++ 17. P. multi-hilo y Concurrencia/View Example 17.0.03/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    static void Tarea()
+    {
+        Console.WriteLine("La tarea ha comenzado");
+        Thread.Sleep(10000);
+        Console.WriteLine("La tarea ha finalizado");
+    }
+
+    static void Reloj(DateTime dateTime)
+    {
+        while (true)
+        {
+            Console.WriteLine($"Tiempo transcurrido{dateTime}");
+            Thread.Sleep(1000);
+        }
+    }
+
+    private static void Main(string[] args)
+    {
+        // METODO 1
+        //Task tareaHiloSecundario = new Task(Tarea);
+        //tareaHiloSecundario.Start();
+        //tareaHiloSecundario.Wait();
+
+        // METODO 2
+        //Action action = () => { Reloj(DateTime.Now); };
+        //Task tareaHiloSecundario = new(action);
+
+        // METODO 3
+        Task tareaHiloSecundario = new(() => { Reloj(DateTime.Now); });
+        tareaHiloSecundario.Start();
+        
+        ConsoleKey teclaPresionada;
+
+        do
+        {
+            teclaPresionada = Console.ReadKey().Key;
+            Console.WriteLine($"Presiono {teclaPresionada}");
+        }
+        while (teclaPresionada != ConsoleKey.Spacebar);
+        Console.WriteLine("El main principal finalizo");
+    }
+}
