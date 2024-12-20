0653679221422bb8b1a56d9e932010f5c825a757
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-20 07:02:47 -0300

    Up

--- 19. Pattern Matching/Library Example 19.0.01/ConstantPattern.cs
+++ 19. Pattern Matching/Library Example 19.0.01/ConstantPattern.cs
@@ -1,4 +1,6 @@
-﻿namespace Library_Example_19._0._01
+﻿using System.Timers;
+
+namespace Library_Example_19._0._01
 {
     public class ConstantPattern
     {
@@ -7,6 +9,20 @@
             object valor = 10;
             if (valor is 10) // Constant Pattern
                 Console.WriteLine("valor es exactamente 10.");
+        }
+
+        public static void Caso02()
+        {
+            object dato = "Hola";
+            string mensaje = dato switch
+            {
+                null => "El valor es nulo",
+                42 => "El valor es 42",
+                "Hola" => "El valos es la cadena 'Hola'",
+                _ => "Otro valor"
+            };
+
+            Console.WriteLine(mensaje);
         }
 
 
--- 19. Pattern Matching/View Example 19.0.01/Program.cs
+++ 19. Pattern Matching/View Example 19.0.01/Program.cs
@@ -1,3 +1,4 @@
 ﻿using Library_Example_19._0._01;
 
 ConstantPattern.Caso1();
+ConstantPattern.Caso02();
