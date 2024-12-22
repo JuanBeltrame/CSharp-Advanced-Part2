1fcbe9bf2a722a0f1c7bca2e500148787f90120e
Juan Beltrame <juan_beltrame@hotmail.com>
2024-12-22 05:55:14 -0300

    Property Pattern

--- 19. Pattern Matching/Library Example 19.0.01/TypePattern.cs
+++ 19. Pattern Matching/Library Example 19.0.01/TypePattern.cs
@@ -1,13 +1,42 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Library_Example_19._0._01
 {
-    internal class TypePattern
+    public class TypePattern
     {
+        public static void PrintLenght(object obj)
+        {
+            if (obj is string str)
+                Console.WriteLine($"The string has {str.Length} characters. ");
+            else if (obj is ICollection collection)
+                Console.WriteLine($"The collection has {collection.Count} items. ");
+        }
+
+        public static void PrintType(object obj)
+        {
+            if (obj is string or int)
+                Console.WriteLine($"Type: {obj.GetType().Name}, Value: {obj}");
+        }
+
+        public static void Caso03(List<object> lista)
+        {
+            foreach (var item in lista)
+            {
+                if (item is int numero)
+                    Console.WriteLine($"Encontré un entero: {numero}");
+                else if (item is string cadena)
+                    Console.WriteLine($"Encontré una cadena con longitud {cadena.Length}");
+                else
+                    Console.WriteLine($"Tipo desconocido: {item.GetType().Name}");
+            }
+        }
+
         
     }
 }
--- 19. Pattern Matching/View Example 19.0.01/Program.cs
+++ 19. Pattern Matching/View Example 19.0.01/Program.cs
@@ -1,4 +1,18 @@
 ﻿using Library_Example_19._0._01;
 
+Console.WriteLine("-----Constant Pattern-----");
 ConstantPattern.Caso01();
 ConstantPattern.Caso02();
+Console.WriteLine();
+
+Console.WriteLine("-----Type Pattern-----");
+TypePattern.PrintLenght("Hello Word");
+TypePattern.PrintLenght(new List<int>[1, 2, 3]);
+TypePattern.PrintType(1);
+TypePattern.PrintType("1");
+TypePattern.Caso03(new List<object> { 42, "C# Rocks!", DateTime.Now });
+
+
+
+
+
