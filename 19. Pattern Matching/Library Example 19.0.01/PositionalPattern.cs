94d6f45c890aa13098a74f75307eb2f61711f474
Juan Beltrame <juan_beltrame@hotmail.com>
2025-01-14 05:20:36 -0300

    Update Pattern Matching

--- 19. Pattern Matching/Library Example 19.0.01/PositionalPattern.cs
+++ 19. Pattern Matching/Library Example 19.0.01/PositionalPattern.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace Library_Example_19._0._01
 {
-    internal class PositionalPattern
+    // declares struct with a single variable named X
+    public readonly struct Coordinates
     {
+        public int X { get; }
+        public int Y { get; }
+
+        // Constructor para la estructura
+        public Coordinates(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        // Método deconstrucción para asignar valores a variables separadas
+        public void Deconstruct(out int x, out int y)
+        {
+            x = X;
+            y = Y;
+        }
     }
 }
